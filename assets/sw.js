67290e8ba2c677eb027db3088dd6a066b8f511d4
Mitja Felicijan <mitja.felicijan@gmail.com>
2018-08-08 14:26:34 +0200

    content update

--- assets/sw.js
+++ assets/sw.js
@@ -4,9 +4,6 @@ self.addEventListener('install', function (e) {
 	e.waitUntil(
 		caches.open('mitjafelicijan').then(function (cache) {
 			return cache.addAll([
-				'/',
-				'/index.html',
-				'/index.html?homescreen=1',
 				'/?homescreen=1',
 				'/assets/site.css',
 			]);
@@ -15,7 +12,6 @@ self.addEventListener('install', function (e) {
 });
 
 self.addEventListener('fetch', function (event) {
-	console.log(event.request.url);
 	event.respondWith(
 		caches.match(event.request).then(function (response) {
 			return response || fetch(event.request);

9c59a70cb01e101264c630b95dcdcd0050ff5573
Mitja Felicijan <mitja.felicijan@gmail.com>
2018-08-08 14:12:28 +0200

    content update

--- assets/sw.js
+++ assets/sw.js
@@ -1,9 +1,8 @@
 importScripts('/assets/cache-polyfill.js');
 
-
 self.addEventListener('install', function (e) {
 	e.waitUntil(
-		caches.open('airhorner').then(function (cache) {
+		caches.open('mitjafelicijan').then(function (cache) {
 			return cache.addAll([
 				'/',
 				'/index.html',
@@ -11,6 +10,15 @@ self.addEventListener('install', function (e) {
 				'/?homescreen=1',
 				'/assets/site.css',
 			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', function (event) {
+	console.log(event.request.url);
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
 		})
 	);
 });

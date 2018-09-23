87c770d4fd8fe4c47a8bef0a9875cf4ade006f8c
Mitja Felicijan <mitja.felicijan@gmail.com>
2018-09-23 23:24:21 +0200

    content update

--- slides/vendor/slides.js
+++ slides/vendor/slides.js
@@ -115,3 +115,27 @@ window.addEventListener('load', function(evt) {
 	}
 
 }, false);
+
+
+// hide mouse after 5seconds
+(function() {
+	var mouseTimer = null,
+		cursorVisible = true;
+
+	function disappearCursor() {
+		mouseTimer = null;
+		document.body.style.cursor = 'none';
+		cursorVisible = false;
+	}
+
+	document.onmousemove = function() {
+		if (mouseTimer) {
+			window.clearTimeout(mouseTimer);
+		}
+		if (!cursorVisible) {
+			document.body.style.cursor = 'default';
+			cursorVisible = true;
+		}
+		mouseTimer = window.setTimeout(disappearCursor, 5000);
+	};
+})();

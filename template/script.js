43e7ed294c18ee8004f40ec6c1ed1c21f1190c49
Mitja Felicijan <mitja.felicijan@gmail.com>
2021-10-07 21:24:39 +0200

    Async JS script

--- template/script.js
+++ template/script.js
@@ -1,17 +1,19 @@
-// dither image on mouse over replace
-document.querySelectorAll('article img').forEach(img => {
+window.addEventListener('load', () => {
+  // dither image on mouse over replace
+  document.querySelectorAll('article img').forEach(img => {
 
-  const ditheredImage = img.src;
-  const originalImage = img.src.replace('.dith.gif', '');
+    const ditheredImage = img.src;
+    const originalImage = img.src.replace('.dith.gif', '');
 
-  img.addEventListener('mouseover', evt => {
-    evt.target.src = originalImage;
-    console.log('mouseover')
+    img.addEventListener('mouseover', evt => {
+      evt.target.src = originalImage;
+      console.log('mouseover')
+    });
+
+    img.addEventListener('mouseout', evt => {
+      evt.target.src = ditheredImage;
+      console.log('mouseout')
+    });
+
   });
-
-  img.addEventListener('mouseout', evt => {
-    evt.target.src = ditheredImage;
-    console.log('mouseout')
-  });
-
 });

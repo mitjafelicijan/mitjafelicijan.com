e9cfc4c35f9904cf1ac3a1fabcd8ec865091307b
Mitja Felicijan <mitja.felicijan@gmail.com>
2022-08-14 17:31:55 +0200

    CV head animation with flipping on X axis

--- template/script.js
+++ template/script.js
@@ -3,13 +3,9 @@ window.addEventListener('load', async () => {
   // flip CV image on mouse over
   const cvImage = document.querySelector('.cv-picture img');
   if (cvImage) {
-    cvImage.addEventListener('mouseover', evt => {
-      evt.target.style.transform = 'scaleX(-1)';
-    });
-
-    cvImage.addEventListener('mouseout', evt => {
-      evt.target.style.transform = 'scaleX(1)';
-    });
+    setInterval(() => {
+      cvImage.style.transform = cvImage.style.transform === 'scaleX(1)' ? 'scaleX(-1)' : 'scaleX(1)';
+    }, 1000);
   }
 
 });
